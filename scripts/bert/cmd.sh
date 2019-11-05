094a5b4c1f190a78f47c92613810094c200032db
Piyush Ghai <ghai.8@osu.edu>
2019-11-05 19:39:01 +0000

    change default val of BOUND to 0

--- scripts/bert/cmd.sh
+++ scripts/bert/cmd.sh
@@ -1,32 +1,31 @@
-export DEBUG=0
+export DEBUG="${DEBUG:-1}"
+export HOST="${HOST:-hosts_32}"
+export NP="${NP:-8}"
+export CKPTDIR="${CKPTDIR:-/fsx/test}"
+export OPTIMIZER="${OPTIMIZER:-lamb2}"
+
 export USE_DOCKER=0
-export HOST=hosts_32
 export OTHER_HOST=hosts_31
 export DOCKER_IMAGE=haibinlin/worker_mxnet:c5fd6fc-1.5-cu90-79e6e8-79e6e8
-export NP=256
 export NCCLMINNRINGS=1
 export TRUNCATE_NORM=1
 export LAMB_BULK=60
 export EPS_AFTER_SQRT=1
-export NO_SHARD=1
+export NO_SHARD=0
 export FORCE_WD=0
 export USE_PROJ=0
 export DTYPE=float16
 export MODEL=bert_24_1024_16
-export CKPTDIR=/bert/ckpts/stage1_32k_base
-export CKPTDIR=~/haibin/tmp/ckpts/stage1_64k_base
-export CKPTDIR=/fsx/bert/stage1_64k_proj
 export DATA_HOME=/data
 export DATA_HOME=~/mxnet-data/bert-pretraining/datasets
 export CKPTINTERVAL=300000000
-export OPTIMIZER=lamb2
 export CLUSHUSER=ec2-user
 export HIERARCHICAL=0
 export EVALINTERVAL=100000000
 export COMMIT=58435d04
 export NO_DROPOUT=0
-export USE_BOUND=1
-export ADJUST_BOUND=1
+export USE_BOUND=0
+export ADJUST_BOUND=0
 export WINDOW_SIZE=2000
 
 if [ "$USE_DOCKER" = "1" ]; then
@@ -40,8 +39,8 @@ sleep 5
 if [ "$DEBUG" = "1" ]; then
     export LOGINTERVAL=1
     export NUMSTEPS=5
-    export OPTIONS='--synthetic_data --verbose'
-    export NUMSTEPS=500000
+    export OPTIONS='--synthetic_data --verbose --eval_use_npz'
+    export NUMSTEPS=5000000000
     export LOGINTERVAL=5
 else
     export LOGINTERVAL=50
@@ -51,7 +50,11 @@ else
 fi
 #BS=65536 ACC=4 MAX_SEQ_LENGTH=128 MAX_PREDICTIONS_PER_SEQ=20 LR=0.006 WARMUP_RATIO=0.2843 bash mul-hvd.sh
 #BS=32768 ACC=2 MAX_SEQ_LENGTH=128 MAX_PREDICTIONS_PER_SEQ=20 LR=0.005 WARMUP_RATIO=0.2 bash mul-hvd.sh
-#BS=64 ACC=1 MAX_SEQ_LENGTH=128 MAX_PREDICTIONS_PER_SEQ=20 LR=0.005 WARMUP_RATIO=0.2 bash mul-hvd.sh
+if [ "$NP" = "1" ]; then
+    BS=64 ACC=1 MAX_SEQ_LENGTH=128 MAX_PREDICTIONS_PER_SEQ=20 LR=0.0001 WARMUP_RATIO=0.2 bash mul-hvd.sh
+elif [ "$NP" = "8" ]; then
+    BS=512 ACC=1 MAX_SEQ_LENGTH=128 MAX_PREDICTIONS_PER_SEQ=20 LR=0.005 WARMUP_RATIO=0.2 bash mul-hvd.sh
+fi
 
 if [ "$USE_DOCKER" = "1" ]; then
     export PORT=12452
